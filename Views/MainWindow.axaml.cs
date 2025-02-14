8673fa61ff161e5696fe995beb983e38eaff4e4f
Ruan <ruanmontelo@gmail.com>
2025-02-13 21:31:22 -0300

    Generating a simple JSON to test the formatting in the TextBox control.

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 using Avalonia.Controls;
 
 namespace Lance.Views;
@@ -7,5 +10,18 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        FillResponseTextBoxWithJsonArray();
     }
+
+    private void FillResponseTextBoxWithJsonArray()
+    {
+        IEnumerable<Login> logins = Enumerable.Range(0, 10).Select(_ => GetLoginObject());
+
+        ResponseContentTextBox.Text =
+            JsonSerializer.Serialize(logins, new JsonSerializerOptions { WriteIndented = true });
+    }
+
+    private static Login GetLoginObject() => new("login", "password");
 }
+
+public record Login(string Username, string Password);
