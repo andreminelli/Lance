7404106c8bd5cf784383e50b84194105fb2eed6e
Ruan <ruanmontelo@gmail.com>
2025-02-16 12:02:11 -0300

    Added new elements, new behavior for pasting content in a request body textbox and changed the http method, uri and "go" components to fit in a minimum of 1280x720 resolution

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
+using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Input.Platform;
+using Avalonia.Interactivity;
 
 namespace Lance.Views;
 
@@ -15,13 +18,37 @@ public partial class MainWindow : Window
 
     private void FillResponseTextBoxWithJsonArray()
     {
-        IEnumerable<Login> logins = Enumerable.Range(0, 10).Select(_ => GetLoginObject());
+        IEnumerable<Login> logins = Enumerable.Range(0, 30).Select(_ => GetLoginObject());
 
         ResponseContentTextBox.Text =
             JsonSerializer.Serialize(logins, new JsonSerializerOptions { WriteIndented = true });
     }
 
     private static Login GetLoginObject() => new("login", "password");
+
+    private void TextBox_OnPastingFromClipboard(object? sender, RoutedEventArgs e)
+    {
+        IClipboard? clipboard = GetTopLevel(this)?.Clipboard;
+        
+        if (clipboard is null)
+            return;
+
+        Task<string?> data = clipboard.GetTextAsync();
+        
+        while (!data.IsCompleted)
+        {
+            if (data.IsCanceled || data.IsFaulted)
+                return;
+        }
+
+        if (sender is not TextBox textBox)
+            return;
+
+        if (string.IsNullOrEmpty(data.Result) || string.IsNullOrWhiteSpace(data.Result))
+            return;
+        
+        textBox.Text = data.Result;
+    }
 }
 
 public record Login(string Username, string Password);
